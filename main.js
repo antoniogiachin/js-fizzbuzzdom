7773ae7f445b3a038532a3a5612b2d51af796c23
antoniogiachin <giachinantonio@outlook.com>
2022-01-21 17:17:04 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const container = document.getElementById('container');
 
 // Creo ciclo di inserimento elementi
-for (i = 1; i <= 1000; i++){
+for (let i = 1; i <= 1000; i++){
     
     console.log(i);
 
