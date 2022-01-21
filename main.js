a40e59d0dc0122cf25059a15fe80df480abf6377
antoniogiachin <giachinantonio@outlook.com>
2022-01-21 16:56:10 +0100

    fine esercizio

---
+++ main.js
@@ -0,0 +1,19 @@
+// Creo costante con cui riferirmi a container
+const container = document.getElementById('container');
+
+// Creo ciclo di inserimento elementi
+for (i = 1; i <= 1000; i++){
+    
+    console.log(i);
+
+    if (i % 3 == 0 && i % 5 ==0) {
+        container.innerHTML += '<div class="box box-both">fizzbuzz</div>';
+    } else if (i % 3 ==0){
+        container.innerHTML += '<div class="box box-three"> fizz</div>'; 
+    } else if (i % 5 ==0){
+        container.innerHTML += '<div class="box box-five">buzz</div>';
+    }  else{
+        container.innerHTML += `<div class="box box-main">${i}</div>`;
+    }                    
+     
+}
